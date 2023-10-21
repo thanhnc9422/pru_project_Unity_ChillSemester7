ce82efb84282acf2cf6c9e57ce3692cea8c5edb1
hachan140 <havu14102@gmail.com>
2023-10-21 22:23:51 +0700

    add game over screen

---
+++ Assets/script/GameOver.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void RetryButton()
+    {
+        SceneManager.LoadScene("SampleScreen");
+    }
+
+    public void ExistButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
